b0ae0802a4cbf679b484a7d39c9d2d1350220a6c
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-04-20 16:57:17 +0500

    feat: added test data

---
+++ src/testJSONS/config-test.ts
@@ -0,0 +1,19 @@
+export const warehouse_objects = {
+    ip: "10.21.1212.1",
+    objects: [
+        {
+            name: "DI-909",
+            client: {name: "Client 104-01", cl_object: "777"},
+            server: {name: "Server 104-01", cl_object: "333"},
+        },
+        {
+            name: "DI-808",
+            client: {name: "Client 104-01", cl_object: "888"},
+            server: {name: "Server 104-02", cl_object: "333"},
+        },
+    ]
+}
+
+
+export const clients = ["Client 104-01", "Client 104-02"]
+export const server = ["Server 104-01", "Server 104-02"]
