e3e766a7e717377d7427cf7a1df2803dbd81474c
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-04-20 14:07:56 +0500

    feat: made dynamic lists

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 import './app.css'
-import { Group, Layer, Rect,  Text, Line} from "react-konva";
+import {Group, Layer, Rect, Text, Line} from "react-konva";
 import React from "react";
 import {Map} from "../components/map/map";
 import {ListItem} from "../components/ui/list-item/list-item";
@@ -21,7 +21,8 @@ export const App = () => {
             <Layer>
                 <Group onDragMove={e => {
                     const position = e.target.position();
-                    setFromCoords({x:  position.x , y: position.y })}} draggable={true} x={fromCoords.x} y={fromCoords.y}>
+                    setFromCoords({x: position.x, y: position.y})
+                }} draggable={true} x={fromCoords.x} y={fromCoords.y}>
                     <Rect width={3000} height={3000} stroke={'black'} strokeWidth={2}>
                     </Rect>
                     <Group draggy={0}
@@ -40,47 +41,43 @@ export const App = () => {
                             fontSize={100}
                         />
                     </Group>
-                    <List x={400} y={400} width={500} height={490}>
+                    <List x={400} y={400}>
                         <ListItem text={'Client 10-04'} width={500} height={70} y={0}/>
-                        <ListItem align={'left'} text={'280'} width={500} height={70} y={70}/>
-                        <ListItem  align={'left'} text={"270"} width={500} height={70} y={140}/>
-                        <ListItem  align={'left'} text={"290"} width={500} height={70} y={210}/>
-                        <ListItem  align={'left'} text={"210"} width={500} height={70} y={280}/>
-                        <ListItem  align={'left'} text={"240"} width={500} height={70} y={350}/>
-                        <ListItem  align={'left'} text={"260"} width={500} height={70} y={420}/>
+                        {['280', "270", "290", "210", "240", "260"].map((el, i) => {
+                            return <ListItem align={'left'} text={el} width={500} height={70} y={(i+1)*70}/>
+                        })}
                     </List>
-                    <List x={1300} y={1100} width={500} height={70 * (joins.warehouse.length + 1)}>
+                    <List x={1300} y={1100}>
                         <ListItem text={'Warehouse'} width={500} height={70} y={0}/>
-                        {joins.warehouse.map((element) =>  {
+                        {joins.warehouse.map((element) => {
                             wr_coord_y = wr_coord_y + 70
                             return <ListItem align={'left'} text={element} width={500} height={70} y={wr_coord_y}/>
                         })}
                     </List>
-                    {joins.warehouse.map((element) =>  {
+                    {joins.warehouse.map((element) => {
                         line_coord_y += 70
-                        return <Line points={[1300, 1135 + line_coord_y, 420+500, 2000+105]} stroke={'white'}
-                              strokeWidth={2}/>
+                        return <Line points={[1300, 1135 + line_coord_y, 420 + 500, 2000 + 105]} stroke={'white'}
+                                     strokeWidth={2}/>
                     })}
-                    <List x={2000} y={1700} width={500} height={210}>
+                    <List x={2000} y={1700}>
                         <ListItem text={'fbd t_me'} width={500} height={70} y={0}/>
-                        <ListItem  align={'left'} text={"04"} width={500} height={70} y={70}/>
-                        <ListItem  align={'left'} text={"05"} width={500} height={70} y={140}/>
+                        {["04","05"].map((el, i) => {
+                            return <ListItem align={'left'} text={el} width={500} height={70} y={(i+1)*70}/>
+                        })}
                     </List>
 
-                    <List x={420} y={2000} width={500} height={490}>
+                    <List x={420} y={2000}>
                         <ListItem text={'Server 10-04'} width={500} height={70} y={0}/>
-                        <ListItem align={'left'} text={'280'} width={500} height={70} y={70}/>
-                        <ListItem  align={'left'} text={"270"} width={500} height={70} y={140}/>
-                        <ListItem  align={'left'} text={"290"} width={500} height={70} y={210}/>
-                        <ListItem  align={'left'} text={"210"} width={500} height={70} y={280}/>
-                        <ListItem  align={'left'} text={"240"} width={500} height={70} y={350}/>
-                        <ListItem  align={'left'} text={"260"} width={500} height={70} y={420}/>
+                        {['280', "270", "290", "210", "240", "260"].map((el, i) => {
+                            return <ListItem align={'left'} text={el} width={500} height={70} y={(i+1)*70}/>
+                        })}
                     </List>
                 </Group>
-                <Group  onDragMove={e => {
+                <Group onDragMove={e => {
                     const position = e.target.position();
-                    setToCoords({x:  position.x , y: position.y })}}
-                 draggable={true} x={toCoords.x} y={toCoords.y}>
+                    setToCoords({x: position.x, y: position.y})
+                }}
+                       draggable={true} x={toCoords.x} y={toCoords.y}>
                     <Rect width={3000} height={3000} stroke={'black'} strokeWidth={2}>
                     </Rect>
 
@@ -100,47 +97,40 @@ export const App = () => {
                             fontSize={100}
                         />
                     </Group>
-                    <List x={400} y={400} width={500} height={490}>
+
+                    <List x={400} y={400}>
                         <ListItem text={'Client 10-04'} width={500} height={70} y={0}/>
-                        <ListItem align={'left'} text={'280'} width={500} height={70} y={70}/>
-                        <ListItem  align={'left'} text={"270"} width={500} height={70} y={140}/>
-                        <ListItem  align={'left'} text={"290"} width={500} height={70} y={210}/>
-                        <ListItem  align={'left'} text={"210"} width={500} height={70} y={280}/>
-                        <ListItem  align={'left'} text={"240"} width={500} height={70} y={350}/>
-                        <ListItem  align={'left'} text={"260"} width={500} height={70} y={420}/>
+                        {['280', "270", "290", "210", "240", "260"].map((el, i) => {
+                            return <ListItem align={'left'} text={el} width={500} height={70} y={(i+1)*70}/>
+                        })}
                     </List>
 
-                    <List x={1300} y={1100} width={500} height={560}>
+                    <List x={1300} y={1100}>
                         <ListItem text={'Warehouse'} width={500} height={70} y={0}/>
-                        <ListItem align={'left'} text={'DI-250'} width={500} height={70} y={70}/>
-                        <ListItem  align={'left'} text={'DI-251'} width={500} height={70} y={140}/>
-                        <ListItem  align={'left'} text={'DI-252'} width={500} height={70} y={210}/>
-                        <ListItem  align={'left'} text={'DI-253'} width={500} height={70} y={280}/>
-                        <ListItem  align={'left'} text={'DI-253'} width={500} height={70} y={350}/>
-                        <ListItem  align={'left'} text={'DI-253'} width={500} height={70} y={420}/>
-                        <ListItem  align={'left'} text={'DI-253'} width={500} height={70} y={490}/>
+                        {['DI-250','DI-251','DI-252','DI-253','DI-253','DI-253','DI-253'].map((el, i) => {
+                            return <ListItem align={'left'} text={el} width={500} height={70} y={(i+1)*70}/>
+                        })}
                     </List>
-                    <List x={2000} y={1700} width={500} height={210}>
+                    <List x={2000} y={1700}>
                         <ListItem text={'fbd t_me'} width={500} height={70} y={0}/>
-                        <ListItem  align={'left'} text={"04"} width={500} height={70} y={70}/>
-                        <ListItem  align={'left'} text={"05"} width={500} height={70} y={140}/>
+                        <ListItem align={'left'} text={"04"} width={500} height={70} y={70}/>
+                        <ListItem align={'left'} text={"05"} width={500} height={70} y={140}/>
                     </List>
-                    <List x={420} y={2000} width={500} height={490}>
+                    <List x={420} y={2000}>
                         <ListItem text={'Server 10-04'} width={500} height={70} y={0}/>
-                        <ListItem align={'left'} text={'280'} width={500} height={70} y={70}/>
-                        <ListItem  align={'left'} text={"270"} width={500} height={70} y={140}/>
-                        <ListItem  align={'left'} text={"290"} width={500} height={70} y={210}/>
-                        <ListItem  align={'left'} text={"210"} width={500} height={70} y={280}/>
-                        <ListItem  align={'left'} text={"240"} width={500} height={70} y={350}/>
-                        <ListItem  align={'left'} text={"260"} width={500} height={70} y={420}/>
+                        {['280', "270", "290", "210", "240", "260"].map((el, i) => {
+                            return <ListItem align={'left'} text={el} width={500} height={70} y={(i+1)*70}/>
+                        })}
                     </List>
                 </Group>
-                { fromCoords.x > toCoords.x &&
-                    <Line points={[fromCoords.x + 400, fromCoords.y + 400, toCoords.x + 900, toCoords.y + 400]} stroke={'white'}
-                          strokeWidth={2}/>}
-                { fromCoords.x < toCoords.x &&
-                    <Line points={[fromCoords.x + 900, fromCoords.y + 400, toCoords.x + 400, toCoords.y + 400]} stroke={'white'}
-                          strokeWidth={2}/>}
+                {fromCoords.x > toCoords.x &&
+                    <Line points={[fromCoords.x + 400, fromCoords.y + 400, toCoords.x + 900, toCoords.y + 400]}
+                          stroke={'red'}
+                          strokeWidth={10}/>}
+                {fromCoords.x < toCoords.x &&
+                    <Line points={[fromCoords.x + 900, fromCoords.y + 400, toCoords.x + 400, toCoords.y + 400]}
+                          stroke={'red'}
+                          strokeWidth={10}/>}
             </Layer>
         </Map>
 
--- src/components/ui/list/list.tsx
+++ src/components/ui/list/list.tsx
@@ -1,20 +1,31 @@
-import React, {FC, ReactElement} from 'react';
-import {Group, Rect} from "react-konva";
+import React, {Children, FC, ReactElement, useMemo} from 'react';
+import {Group, Rect, Text} from "react-konva";
 
 
 type TList = {
     x: number
     y: number
-    width: number
-    height: number
     children: ReactElement | ReactElement[] | any
 }
 
-export const List: FC<TList> = ({children, y, height, x, width }) => {
+export const List: FC<TList> = ({children, y, x }) => {
+    const liHeight = useMemo(() => {
+        return Children.map(children,el => el.props.height)[0]
+    },[children])
+
+    const liWidth = useMemo(() => {
+        return Children.map(children,el => el.props.width)[0]
+    },[children])
+
+    const length = useMemo(() => {
+        return Children.count(children)
+    }, [children])
+
     return (
         <Group draggable={false} x={x} y={y}>
-            <Rect width={width} height={height} stroke={'black'} strokeWidth={2}>
+            <Rect width={liWidth} height={length * liHeight} stroke={'black'} strokeWidth={2}>
             </Rect>
+            <Text text={`${length}`} width={100} fontSize={80}></Text>
             {children}
         </Group>
     );
