1af9775a21e05e2967886c6856072d6a19f80f1f
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-04-20 21:19:53 +0500

    feat: added test controllers config

---
+++ src/testJSONS/config-connection.ts
@@ -0,0 +1,21 @@
+export const controllers = [
+  {
+    ip: "1",
+    links: {
+      "2": {
+        clientToServer: [
+          [
+            { name: "Client 104-01", cl_object: "777" },
+            { name: "Server 104-01", cl_object: "333" },
+          ],
+        ],
+        serverToClient: [
+          [
+            { name: "Server 104-01", cl_object: "333" },
+            { name: "Client 104-01", cl_object: "777" },
+          ],
+        ],
+      },
+    },
+  },
+]
