f2e93d8c1a4e3264c678db0fa6dd39de5f73d904
AricsyGIT <aricsybsn@gmail.com>
2023-04-21 02:05:22 +0500

    feat: добавил возможность менять связи между контроллерами

--- src/testJSONS/config-connection.ts
+++ src/testJSONS/config-connection.ts
@@ -8,13 +8,13 @@ export const controllers = [
           // первый - рассматриваемый котроллер, второй - следующий или предыдущий
           [
             { name: "Client 104-01", cl_object: "777" }, // массив кортежей двух элементов
-            { name: "Server 104-01", cl_object: "777" },
+            { name: "Server 204-01", cl_object: "222" },
           ],
         ],
         serverToClient: [
           [
             { name: "Server 104-02", cl_object: "333" },
-            { name: "Client 104-01", cl_object: "777" },
+            { name: "Client 204-02", cl_object: "333" },
           ],
         ],
       },
