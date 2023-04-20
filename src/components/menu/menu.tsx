7902278503f075999580fd7d4e3e51ae69ba2860
Danil Gribanov <gribanovwork@yandex.ru>
2023-04-20 15:47:06 +0500

    feat: закинул запаршенные jsons

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -2,9 +2,10 @@ import {createPortal} from "react-dom";
 import {DropDownList} from "../propdown-list/dropdown-list-item";
 import serv from  "../../jsons/test/res_parse_controller_3_server (2).json"
 import './menu.css'
+import {getControllerList} from "../../utils/getControllerList";
 
 export const Menu = () => {
-
+    getControllerList()
     return createPortal(
             <div className="menu">
                 <DropDownList DropdownList={serv} name={""} />
---
+++ src/utils/getControllerList.ts
@@ -0,0 +1,8 @@
+
+
+export const getControllerList = () => {
+    // let test = require("../jsons/test")
+    // console.log(JSONS)
+    // var filename = url.split('/').pop()
+
+}
